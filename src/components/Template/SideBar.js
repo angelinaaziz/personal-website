54695b22cc2291b9984b8d23a4609810d09cc27f
Angelina Aziz <angelinaaziz1@gmail.com>
2023-11-05 17:18:16 +0000

    updating the site with L&G info

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,7 +19,7 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Angelina. I am a software engineer at <a href="https://anaplan.com">Anaplan</a> in London.
+      <p>Hi, I&apos;m Angelina. I am a software engineer at <a href="https://anaplan.com">Legal & General</a> in London.
         Currently pursuing a masters in Systems Engineering at <a href="https://www.lboro.ac.uk">Loughborough University</a>.
         I am the co-founder of <a href="https://www.auralyze.ai">auralyze.ai</a> an AI university interview prep platform.
       </p>
